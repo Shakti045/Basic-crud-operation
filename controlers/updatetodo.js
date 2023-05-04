346994f5d6d485a6b847a79af328826654d4b969
Shakti045 <shaktimandal045@gmail.com>
2023-05-04 16:18:02 +0530

    updated

--- controlers/updatetodo.js
+++ controlers/updatetodo.js
@@ -3,8 +3,9 @@ exports.updatetodo=async (req,res)=>{
     try{
         const {id}=req.params;
 
-        const {title}=req.body;
-         await  TODO.findByIdAndUpdate({_id:id},{"title":title})
+        const {title,description}=req.body;
+
+         await  TODO.findByIdAndUpdate({_id:id},{"title":title,"description":description,"updatedat":Date.now()})
         res.status(200).json({
             "message":`Todo for id ${id} updated successfully`
         })
